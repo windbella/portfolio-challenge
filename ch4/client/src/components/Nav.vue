467e2d26346ed6fd77a38d30e6b24983fedb0dd1
windbell <workthread.start@gmail.com>
2020-04-15 22:42:42 +0900

    로고 파일 위치 변경

--- ch4/client/src/components/Nav.vue
+++ ch4/client/src/components/Nav.vue
@@ -3,7 +3,7 @@
         <div class="navbar is-primary" role="navigation" aria-label="main navigation">
             <div class="navbar-brand">
                 <a class="navbar-item" href="/">
-                    <img src="/logo.png" width="28" height="28">
+                    <img src="../assets/logo.png" width="28" height="28">
                 </a>
 
                 <a
