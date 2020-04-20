7687f922fc690d8f6d113e5e5c7da0ae0d7e93c4
windbell <workthread.start@gmail.com>
2020-04-20 21:38:41 +0900

    ch6 초기화

---
+++ ch6/client/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="app">
+        <Nav />
+        <section class="pf-content">
+            <router-view/>
+        </section>
+    </div>
+</template>
+
+<script>
+import Nav from './components/Nav.vue';
+
+export default {
+    name: 'App',
+    components: {
+        Nav,
+    },
+};
+</script>
+
+<style lang="css">
+    @media screen and (min-width: 1024px) {
+        .pf-content {
+            width: 1024px;
+            margin: 0 auto;
+        }
+    }
+</style>
---
+++ ch6/client/src/views/About.vue
@@ -0,0 +1,14 @@
+<template>
+    <article class="hero is-fullheight-with-navbar">
+        <div class="hero-body">
+            <div class="container">
+                <h1 class="title">
+                    About
+                </h1>
+                <h2 class="subtitle">
+                    This is an about page
+                </h2>
+            </div>
+        </div>
+    </article>
+</template>
