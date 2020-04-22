49b241b0869f066e6487091a2845327d6dc310c9
windbell <workthread.start@gmail.com>
2020-04-22 22:18:41 +0900

    ch5.1로 변경

--- ch5.1/client/src/views/Home.vue
+++ ch5.1/client/src/views/Home.vue
@@ -1,14 +1,7 @@
 <template>
-    <article class="hero is-fullheight-with-navbar">
-        <div class="hero-body">
-            <div class="container">
-                <h1 class="title">
-                    Home
-                </h1>
-                <h2 class="subtitle">
-                    This is an home page
-                </h2>
-            </div>
+    <div class="container">
+        <div class="notification">
+            This is a home page.
         </div>
-    </article>
+    </div>
 </template>
