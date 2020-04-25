a8623294406cabf831337110541a2a9b3f9f35e2
windbella <workthread.start@gmail.com>
2020-04-25 20:49:03 +0900

    ch6 자료 테스트

--- ch6/client/src/views/Work.vue
+++ ch6/client/src/views/Work.vue
@@ -3,5 +3,25 @@
         <div class="notification">
             This is a work page
         </div>
+        <div class="notification" v-for="item in list" :key="item.key">
+            list {{item.key}}
+        </div>
     </div>
 </template>
+
+<script>
+export default {
+    name: 'Work',
+    data() {
+        return {
+            list: [
+                { key: 1 },
+                { key: 2 },
+                { key: 3 },
+                { key: 4 },
+                { key: 5 },
+            ],
+        };
+    },
+}
+</script>
