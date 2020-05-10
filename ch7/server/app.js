351c7d6d490b2d439bbd384d0507f94f6065b6ce
windbell <workthread.start@gmail.com>
2020-05-10 22:08:16 +0900

    ch7 sqlite3 설치 및 테스트

--- ch7/server/app.js
+++ ch7/server/app.js
@@ -3,10 +3,28 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const sqlite3 = require('sqlite3').verbose();
 
 const indexRouter = require('./routes/index');
 
 const app = express();
+
+const db = new sqlite3.Database(path.join(__dirname, 'data/db/portfolio.db'));
+
+db.serialize(() => {
+    // db.run('CREATE TABLE lorem (info TEXT)');
+
+    // const stmt = db.prepare('INSERT INTO lorem VALUES (?)');
+    // for (let i = 0; i < 10; i += 1) {
+    //     stmt.run(`Ipsum  ${i}`);
+    // }
+    // stmt.finalize();
+
+    // db.each('SELECT rowid AS id, info FROM lorem', (err, row) => {
+    //     console.log(`${row.id}: ${row.info}`);
+    // });
+});
+db.close();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
