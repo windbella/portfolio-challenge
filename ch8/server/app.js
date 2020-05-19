4d9d75bd56a73fce92d27e40bec7869910401f26
windbell <workthread.start@gmail.com>
2020-05-19 23:12:53 +0900

    ch8 server env 설정

--- ch8/server/app.js
+++ ch8/server/app.js
@@ -2,14 +2,21 @@ const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 const logger = require('morgan');
+const dotenv = require('dotenv');
 const routes = require('./routes');
-
 const dbHelper = require('./utilities/db-helper');
 const fileHelper = require('./utilities/file-helper');
 
 const Work = require('./models/work');
 const File = require('./models/file');
+
+if (process.env.NODE_ENV === 'production') {
+    dotenv.config({ path: path.join(__dirname, '.env.production') });
+} else {
+    dotenv.config({ path: path.join(__dirname, '.env.development') });
+}
 
 dbHelper.initialize(path.join(__dirname, './data/db/portfolio.db'), [File.schema, Work.schema]);
 fileHelper.initialize(path.join(__dirname, './data/files'));
@@ -20,15 +27,15 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+if (process.env.EXPRESS_APP_CORS === 'true') {
+    app.use(cors());
+}
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../client', 'dist'), { index: false }));
-app.use('*', (_, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    next();
-});
 app.use('/uploads', express.static(fileHelper.dirPath));
 app.use('/', routes);
 
